f555cc7c78a52b66647686a3a066f48159db8a34
Saisree Andavarapu <saisreeandavarapu@gmail.com>
2026-03-28 17:22:53 +0530

    fix some issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Menu, X, GraduationCap } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Navbar = () => {
